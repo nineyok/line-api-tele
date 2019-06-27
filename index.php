a0db282ee919c35a5f1057ecc7e48469e943a24f
nineyok <38936997+nineyok@users.noreply.github.com>
2019-06-27 17:35:34 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -265,26 +265,14 @@ $num=0;
 
 function send_reply_message($url, $post_header, $post_body)
 {
-/*     $ch = curl_init($url);
+    $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    //curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
     $result = curl_exec($ch);
-    curl_close($ch); */
-	
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL,$url);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt($ch, CURLOPT_HEADER, false);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $POST_HEADER);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-$result = curl_exec($ch);
-curl_close ($ch);
+    curl_close($ch);
 
     return $result;
 }
