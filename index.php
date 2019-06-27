b22bdaa81dc2226058cb07e18f835d44495b09d8
nineyok <38936997+nineyok@users.noreply.github.com>
2019-06-27 21:03:13 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -11,7 +11,49 @@ $arrayloop = array();
 if($strchk[0]=="$"){
   $arrstr  = explode( "$" , $strexp );
   for($k=1 ; $k < count( $arrstr ) ; $k++ ){
-$jsonFlex = [
+	   $strchk = "$".$arrstr[$k];
+      $idcard = substr($strchk,1);
+      $chkid = substr($idcard,0,10);
+	   if(is_numeric($chkid)){
+              $countid = strlen($chkid);
+              if($countid == "10"){
+                $idcard = $chkid;
+              }
+            }
+	  if(is_numeric($idcard)){
+	     if ($idcard != "") {
+     $urlWithoutProtocol = "http://vpn.idms.pw/id_pdc/selecttel.php?uid=".$idcard;	 
+     $isRequestHeader = FALSE;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $urlWithoutProtocol);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $productivity = curl_exec($ch);
+        curl_close($ch);
+        //$json_a = json_decode($productivity, true);
+        $arrbn_id = explode("#", $productivity);
+        //print_r($arrbn_id);
+//        if (is_numeric(substr($arrbn_id[0], 0, 1))) {
+
+//        echo $objResult["customer_name"];
+//        echo "#" . $objResult["Latitude"];
+//        echo "#" . $objResult["Longitude"];
+//        echo "#" . $objResult["province"];
+//        echo "#" . $objResult["contact_tel"];
+
+
+
+        $Mobile_Number = $arrbn_id[0]; //เบอร์โทร
+	    $Service_Type = $arrbn_id[1]; //เครือข่าย
+        $Start_date = $arrbn_id[2]; // วันที่
+		$Real_Service_Amount = $arrbn_id[3];  //จำนวนเงิน
+        $Topup_Name = substr($arrbn_id[4], 2); // รหัสตู้
+		$customer_name = $arrbn_id[5]; // ชื่อ
+		$latitude = $arrbn_id[6]; // lat
+		$longitude = $arrbn_id[7]; // lon
+        $addresscustomer = $arrbn_id[8]; // ที่อยู่
+	  
+	  
+$txt = [
     "type" => "flex",
     "altText" => "Result",
     "contents" => [
@@ -36,7 +78,7 @@ $jsonFlex = [
               ],
               [
                   "type"=> "text",
-                  "text"=> "0983628932",
+                  "text"=> $Mobile_Number,
                   "flex"=> 5,
                   "size"=> "sm",
                   "wrap"=> true
@@ -58,7 +100,7 @@ $jsonFlex = [
               ],
               [
                   "type"=> "text",
-                  "text"=> "20.00 บาท",
+                  "text"=> $Real_Service_Amount." บาท",
                   "flex"=> 5,
                   "size"=> "sm",
                   "wrap"=> true
@@ -80,7 +122,7 @@ $jsonFlex = [
                 ],
                 [
                   "type"=> "text",
-                  "text"=> "TRUE".$k,
+                  "text"=> $Service_Type,
                   "flex"=> 5,
                   "size"=> "sm",
                   "wrap"=> true
@@ -102,7 +144,7 @@ $jsonFlex = [
                 ],
                 [
                   "type"=> "text",
-                  "text"=> "20-06-2019 05:10:21",
+                  "text"=> $Start_date,
                   "flex"=> 5,
                   "size"=> "sm",
                   "wrap"=> true
@@ -124,7 +166,7 @@ $jsonFlex = [
                 ],
                 [
                   "type"=> "text",
-                  "text"=> "118157",
+                  "text"=> $Topup_Name,
                   "flex"=> 5,
                   "size"=> "sm",
                   "wrap"=> true
@@ -146,7 +188,7 @@ $jsonFlex = [
                 ],
                 [
                   "type"=> "text",
-                  "text"=> "กนกวรรณ งามเสมอ",
+                  "text"=> $customer_name,
                   "flex"=> 5,
                   "size"=> "sm",
                   "wrap"=> true
@@ -168,7 +210,7 @@ $jsonFlex = [
             ],
             [
               "type"=> "text",
-              "text"=> "39/2 ม.7  ต.แหลมบัว อ.นครชัยศรี จ.นครปฐม",
+              "text"=> $addresscustomer,
               "flex"=> 5,
               "size"=> "sm",
               "wrap"=> true
@@ -197,7 +239,7 @@ $jsonFlex = [
               "action"=> [
                 "type"=> "uri",
                 "label"=> "Location",
-                "uri"=> "https://www.google.co.th/maps/place/13.86767822,100.1506225"
+                "uri"=> "https://www.google.co.th/maps/place/".$latitude.",".$longitude.""
               ]
             ]
           ]
@@ -206,9 +248,89 @@ $jsonFlex = [
       ]
     ]
   ];
-    $arrPostData = array();
-    $arrPostData["detail"] = $jsonFlex;
-array_push($arrayloop,$arrPostData);
+  
+  if($Topup_Name!=""){
+                      $arrPostData = array();
+                      //$arrPostData["idcard"] = $idcard;
+                      $arrPostData["detail"] = $txt;
+                      //$arrPostData["status"] = $status;
+                      array_push($arrayloop,$arrPostData);
+                  }else{			
+					$txt = [
+    "type" => "flex",
+    "altText" => "Result",
+    "contents" => [
+      "type" => "bubble",
+      "direction" => "ltr",
+      "body" => [
+        "type" => "box",
+        "layout" => "vertical",
+        "contents" => [
+           [
+            "type" => "box",
+            "layout" => "baseline",
+            "margin" => "sm",
+            "contents" => [
+              [
+                  "type"=> "text",
+                  "text"=> "ไม่พบข้อมูลที่ค้นหา : ".$idcard,
+                  "flex"=> 3,
+                  "size"=> "sm",
+                  "weight"=> "bold",
+                  "color"=> "#FF0000"
+              ]
+				  ]
+				  ]
+				  ]
+				  ]
+				  ]
+				  ];
+                      
+                      $arrPostData = array();
+                      //$arrPostData["idcard"] = $idcard;
+                      $arrPostData["detail"] = $txt;
+                      //$arrPostData["status"] = "0";
+                      array_push($arrayloop,$arrPostData);
+                  }
+    }
+  }else{
+	  
+	  $txt = [
+    "type" => "flex",
+    "altText" => "Result",
+    "contents" => [
+      "type" => "bubble",
+      "direction" => "ltr",
+      "body" => [
+        "type" => "box",
+        "layout" => "vertical",
+        "contents" => [
+           [
+            "type" => "box",
+            "layout" => "baseline",
+            "margin" => "sm",
+            "contents" => [
+              [
+                  "type"=> "text",
+                  "text"=> "ไม่พบข้อมูลที่ค้นหา : ".$idcard,
+                  "flex"=> 3,
+                  "size"=> "sm",
+                  "weight"=> "bold",
+                  "color"=> "#FF0000"
+              ]
+				  ]
+				  ]
+				  ]
+				  ]
+				  ]
+				  ];
+	  
+                  $arrPostData = array();
+                  //$arrPostData["idcard"] = $idcard;
+                  $arrPostData["detail"] = $txt;
+                  //$arrPostData["status"] = "0";
+                  array_push($arrayloop,$arrPostData);
+              }
   }
 }
 $arrPostData = array();
