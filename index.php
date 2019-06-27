71cdb206f344842c24c028208030c08d73a50b6c
nineyok <38936997+nineyok@users.noreply.github.com>
2019-06-27 16:32:35 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -209,8 +209,7 @@ $jsonFlex = [
 
 
 
-if ( sizeof($request_array['events']) > 0 ) {
-    foreach ($request_array['events'] as $event) {
+
         error_log(json_encode($event));
         $reply_message = '';
         $reply_token = $event['replyToken'];
@@ -226,13 +225,6 @@ if ( sizeof($request_array['events']) > 0 ) {
         $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
 
         $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
-
-        echo "Result: ".$send_result."\r\n";
-        
-    }
-}
-
-echo "OK";
 
 
 
